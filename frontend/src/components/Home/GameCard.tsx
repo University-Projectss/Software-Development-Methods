6a46fa55821d48454350c8bbc5c1a4c1255013d5
rob3rtu <robertudrea2002@yahoo.ro>
2023-04-06 20:38:27 +0300

    some changes on homepage

---
+++ frontend/src/components/Home/GameCard.tsx
@@ -0,0 +1,49 @@
+import {
+  Button,
+  ButtonGroup,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { NavBar } from "../NavBar";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+
+interface GameCardProps {
+  title: string;
+  description: string;
+  rating: number;
+}
+
+export const GameCard: React.FC<GameCardProps> = ({
+  title,
+  description,
+  rating,
+}) => {
+  return (
+    <Card maxW="sm" height={300}>
+      <CardBody position="relative">
+        <Stack spacing="3">
+          <Heading size="md">{title}</Heading>
+          <Text>{description}</Text>
+          <Text
+            color="blue.600"
+            fontSize="2xl"
+            position="absolute"
+            bottom={2}
+            left={5}
+          >
+            {rating}/5
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Button variant="solid" colorScheme="blue">
+          Play now
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,20 +1,56 @@
-import { Flex, Text } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { NavBar } from "../NavBar";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+import { GameCard } from "./GameCard";
 
 export const HomeComponent = () => {
   return (
     <>
       <NavBar selection={1} />
       <Flex
+        direction="column"
         backgroundColor="#222222"
         height="90vh"
         width="100vw"
-        justify="center"
+        align="center"
+        justify="space-between"
         paddingTop="5"
+        paddingBottom="150"
       >
         <Text color="#F3EFE0" fontSize={40}>
           This is where the fun begins!
         </Text>
+
+        <Flex width="100%" justify="space-evenly" align="center">
+          <GameCard
+            title="Tic Tac Toe"
+            description="Tic Tac Toe is a classic two-player game played on a 3x3 grid. Each
+            player takes turns marking a square with their symbol, either an 'X'
+            or an 'O'."
+            rating={4}
+          />
+
+          <GameCard
+            title="Connect Four"
+            description="Connect Four is a two-player game where the players take turns
+            dropping colored discs into a vertical board."
+            rating={3}
+          />
+
+          <GameCard
+            title="Space Invaders"
+            description="A custom space invaders with a Star Wars theme. You have to defend the Millenium Falcon against TIE Fighters."
+            rating={5}
+          />
+        </Flex>
       </Flex>
     </>
   );
